24b23c405d4c7533bc8a9647c5491d19033aaec8
Miles Matthias <miles.matthias@gmail.com>
2012-02-15 22:58:19 -0600

    Big push. Nearly everything is done, except for replacing carousel placeholder pictures and captions.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,6 +2,34 @@
 $(function(){
 
 	$('.carousel').carousel();
-	
+	$('.navbar').scrollspy();
+
+
+	var formPopoverOptions = {
+		trigger: 'focus'
+	};
+	$('#subjectInput').popover(formPopoverOptions);
+	$('#bodyInput').popover(formPopoverOptions);
+
+	var tooltipOptions = {
+		placement: 'bottom'	
+	};
+	$('a#github').tooltip(tooltipOptions);
+	$('a#tumblr').tooltip(tooltipOptions);
+	$('a#twitter').tooltip(tooltipOptions);
+	$('a#email').tooltip(tooltipOptions);
+
+
+	$('#submitBtn').click(function(e){
+		var subject = $('#subjectInput').val();
+		var body = $('#bodyInput').val();
+
+		var mailTo = 'mailto:miles.matthias@gmail.com?';
+		mailTo += 'subject='+subject;
+		mailTo += '&body='+body;
+		
+		window.open(encodeURI(mailTo));
+	});
+
 })
 }(window.jQuery)
